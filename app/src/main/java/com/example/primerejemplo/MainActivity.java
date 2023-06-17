6f07d2a97fdffcdb53e4452018e42e0c7bb94f98
Raul <castillosteven09@outlook.es>
2023-06-17 11:42:18 -0500

    Todos los controles y datos

--- app/src/main/java/com/example/primerejemplo/MainActivity.java
+++ app/src/main/java/com/example/primerejemplo/MainActivity.java
@@ -2,14 +2,32 @@ package com.example.primerejemplo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     //crear el edittext
-    private EditText txtCedula;
+    private EditText txtCedula ;
+    private EditText txtNombre ;
+    private EditText txtCorreo ;
+    private EditText txtTelefono ;
+    private EditText txtCiudad ;
+    private TextView txtFecha ;
+    private RadioButton RdHombre ;
+    private RadioButton RdMujer;
+    //Para el datapicker
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +35,79 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //relacionar la variable del boton con el boton xd
-        txtCedula = (EditText)findViewById(R.id.txtCedula);
+         txtCedula = (EditText)findViewById(R.id.txtCedula);;
+
+        txtNombre = (EditText)findViewById(R.id.txtNombres);
+        txtCorreo = (EditText)findViewById(R.id.txtCorreo);
+        txtTelefono = (EditText)findViewById(R.id.txtTelefono);
+        txtCiudad = (EditText)findViewById(R.id.txtCiudad);
+        txtFecha = (TextView)findViewById(R.id.txtFecha);
+        RdHombre = (RadioButton)findViewById(R.id.rbHombre);
+        //relacionar variables con los botones y textos
+
+        //evento click del editbox
+        txtFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+        //Pasar la fecha al textview
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                String date = month+"/"+dayOfMonth+"/"+year;
+                txtFecha.setText(date);
+            }
+        };
     }
 
     //Metodo para ejecutar el boton enviar
     public void Enviar(View view){
         //Se crea una instancia de la actividad que se quiere abrir
         Intent i = new Intent(this, MainActivity2.class);
+
+
+        String Genero = RdHombre.isChecked()?"Hombre":"Mujer";
+
         //se envia el dato a la siguiente actividad
         i.putExtra("cedula",txtCedula.getText().toString());
+        i.putExtra("nombres",txtNombre.getText().toString());
+        i.putExtra("correo",txtCorreo.getText().toString());
+        i.putExtra("telefono",txtTelefono.getText().toString());
+        i.putExtra("ciudad",txtCiudad.getText().toString());
+        i.putExtra("Fecha",txtFecha.getText().toString());
+        i.putExtra("genero",Genero);
         //se inicia la actividad
         startActivity(i);
+    }
+    public void Limpiar(View view){
+        txtCedula = (EditText)findViewById(R.id.txtCedula);;
+        txtNombre = (EditText)findViewById(R.id.txtNombres);
+        txtCorreo = (EditText)findViewById(R.id.txtCorreo);
+        txtTelefono = (EditText)findViewById(R.id.txtTelefono);
+        txtCiudad = (EditText)findViewById(R.id.txtCiudad);
+        txtFecha = (TextView)findViewById(R.id.txtFecha);
+        RdMujer = (RadioButton)findViewById(R.id.rbMujer);
+        txtCedula.setText("");
+        txtNombre.setText("");
+        txtCorreo.setText("");
+        txtTelefono.setText("");
+        txtCiudad.setText("");
+        txtFecha.setText("");
+        RdHombre.setChecked(false);
+        RdMujer.setChecked(false);
     }
 }
--- app/src/main/java/com/example/primerejemplo/MainActivity2.java
+++ app/src/main/java/com/example/primerejemplo/MainActivity2.java
@@ -9,6 +9,12 @@ public class MainActivity2 extends AppCompatActivity {
 
     //crear una instacia del textview con el mismo id
     private TextView lblCedula;
+    private TextView lblNombres;
+    private TextView lblCorreo;
+    private TextView lblTelonfo;
+    private TextView lblCiudad;
+    private TextView lblFecha;
+    private TextView lblGenero;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +22,21 @@ public class MainActivity2 extends AppCompatActivity {
 
         //se crea la relacion de la instacia con el textview
         lblCedula = (TextView)findViewById(R.id.lblCedula);
+        lblNombres = (TextView)findViewById(R.id.lblNombres);
+        lblCorreo = (TextView)findViewById(R.id.lblCorreo);
+        lblTelonfo = (TextView)findViewById(R.id.lblTelefono);
+        lblCiudad = (TextView)findViewById(R.id.lblCiudad);
+        lblFecha = (TextView)findViewById(R.id.lblFecha);
+        lblGenero = (TextView)findViewById(R.id.lblGenero);
 
-        String dato = getIntent().getStringExtra("cedula");
-        lblCedula.setText("cedula: " + dato);
+        lblCedula.setText("Cedula : " + getIntent().getStringExtra("cedula"));
+        lblNombres.setText("Nombres : " + getIntent().getStringExtra("nombres"));
+        lblCorreo.setText("Correo : " + getIntent().getStringExtra("correo"));
+        lblTelonfo.setText("Telefono : " + getIntent().getStringExtra("telefono"));
+        lblCiudad.setText("Ciudad : " + getIntent().getStringExtra("ciudad"));
+        lblFecha.setText("Fecha : " + getIntent().getStringExtra("Fecha"));
+        lblGenero.setText("Genero : " + getIntent().getStringExtra("genero"));
+
+
     }
 }
